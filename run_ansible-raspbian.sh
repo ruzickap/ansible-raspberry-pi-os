69616dd84b6c361bdf4dee00ebf7b3f9ea3112b1
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-07-28 15:29:27 +0200

    feat: add grafana, prometheus, emqx, zigbee2mqtt, esphome (#186)

--- run_ansible-raspbian.sh
+++ run_ansible-raspbian.sh
@@ -2,6 +2,13 @@
 
 set -eux
 
+# Dues to prometheus.prometheus role
+# https://github.com/ansible/ansible/issues/32499
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+# Used by grafana debug
+export CI=true
+
 cd ansible
 ansible-galaxy install -r requirements.yml
 ansible-playbook --diff --user root -i inventory/hosts main.yml
