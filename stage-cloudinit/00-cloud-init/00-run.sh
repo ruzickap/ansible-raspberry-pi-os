df22f3f3ef91cceacdb77ee7bd46a51e948c1a14
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-13 21:49:02 +0200

    chore: remove installation of meta-data file from cloud-init script

--- stage-cloudinit/00-cloud-init/00-run.sh
+++ stage-cloudinit/00-cloud-init/00-run.sh
@@ -8,7 +8,6 @@ install -m 644 files/99_nocloud.cfg "${ROOTFS_DIR}/etc/cloud/cloud.cfg.d/99_nocl
 # Install cloud-init user-data and meta-data to boot partition
 mkdir -p "${ROOTFS_DIR}/boot/firmware"
 install -m 600 files/user-data "${ROOTFS_DIR}/boot/firmware/user-data"
-install -m 600 files/meta-data "${ROOTFS_DIR}/boot/firmware/meta-data"
 
 # Generate network-config with WiFi credentials
 : "${WIFI_SSID:?WIFI_SSID not set}"
