3da31def7a6fb7b75170f0c739e7fae57865fb43
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-13 18:42:52 +0200

    chore: add cloud-init scripts for Raspberry Pi image setup, including user-data, network configuration, and NoCloud datasource

--- stage-cloudinit/00-cloud-init/00-run.sh
+++ stage-cloudinit/00-cloud-init/00-run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Install cloud-init configuration
 install -m 644 files/99_nocloud.cfg "${ROOTFS_DIR}/etc/cloud/cloud.cfg.d/99_nocloud.cfg"
 
+# Install cloud-init user-data and meta-data to boot partition
 install -m 600 files/user-data "${BOOTFS_DIR}/user-data"
 install -m 600 files/meta-data "${BOOTFS_DIR}/meta-data"
 
+# Generate network-config with WiFi credentials
 : "${WIFI_SSID:?WIFI_SSID not set}"
-: "${WIFI_PSK:?WIFI_PSK not set}"
+: "${WIFI_PASSWORD:?WIFI_PASSWORD not set}"
+export WIFI_PSK="${WIFI_PASSWORD}"
 envsubst < files/network-config > "${BOOTFS_DIR}/network-config"
 chmod 600 "${BOOTFS_DIR}/network-config"
