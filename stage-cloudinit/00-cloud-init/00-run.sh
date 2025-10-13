89053e94928ee56f9781b21e0464ba97c047c470
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-13 21:05:33 +0200

    chore: create firmware directory for cloud-init user-data and meta-data installation

--- stage-cloudinit/00-cloud-init/00-run.sh
+++ stage-cloudinit/00-cloud-init/00-run.sh
@@ -5,10 +5,8 @@ set -euxo pipefail
 mkdir -p "${ROOTFS_DIR}/etc/cloud/cloud.cfg.d"
 install -m 644 files/99_nocloud.cfg "${ROOTFS_DIR}/etc/cloud/cloud.cfg.d/99_nocloud.cfg"
 
-ls -la "${ROOTFS_DIR}"
-ls -la "${ROOTFS_DIR}/boot/"
-
 # Install cloud-init user-data and meta-data to boot partition
+mkdir -p "${ROOTFS_DIR}/boot/firmware"
 install -m 600 files/user-data "${ROOTFS_DIR}/boot/firmware/user-data"
 install -m 600 files/meta-data "${ROOTFS_DIR}/boot/firmware/meta-data"
 
