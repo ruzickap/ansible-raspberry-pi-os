31cfaaee6ce90b50dd44956d6aba4f05be514a24
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-21 17:18:42 +0200

    chore: remove deprecated network configuration scripts

--- stage-my2/00-cloud-init/00-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-install -m 600 files/90_network-config.cfg "${ROOTFS_DIR}/etc/cloud/cloud.cfg.d/"
--- stage-my3/00-networkmanager/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-install -m 600 files/00-network-manager-all.yaml "${ROOTFS_DIR}/lib/netplan/"
-install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
-install -m 644 files/wifi-powersave-off.conf "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
