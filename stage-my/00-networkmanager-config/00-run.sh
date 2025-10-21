21dab89aef1d7ae3755ff4b6bc9826b565c5bab1
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-21 11:16:59 +0200

    chore: add wifi-powersave-off configuration

--- stage-my/00-networkmanager-config/00-run.sh
+++ stage-my/00-networkmanager-config/00-run.sh
@@ -2,3 +2,4 @@
 set -euxo pipefail
 
 install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
+install -m 644 files/wifi-powersave-off.conf "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
