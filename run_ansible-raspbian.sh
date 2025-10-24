0da6d225632cd2ebce429f92482dd189922fd639
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-10-24 10:04:05 +0200

    chore: update Ansible playbooks and configurations
    - Add new Ansible collections in `main.yml` for enhanced functionality.
    - Update Grafana and Prometheus versions in `requirements.yml`.

--- run_ansible-raspbian.sh
+++ run_ansible-raspbian.sh
@@ -4,10 +4,10 @@ set -eux
 
 # Dues to prometheus.prometheus role
 # https://github.com/ansible/ansible/issues/32499
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+# export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
 # Used by grafana debug
-export CI=true
+# export CI=true
 
 cd ansible || exit
 ansible-galaxy install -r requirements.yml
