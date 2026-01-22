ddf7895ac3822f2f0f376f44293ba3182af71b2c
Petr Ruzicka <petr.ruzicka@gmail.com>
2026-01-22 22:20:26 +0100

    refactor: streamline firmware enabling script

--- stage-my/01-nonfree-firmware/00-run.sh
+++ stage-my/01-nonfree-firmware/00-run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-echo "Enabling contrib, non-free and non-free-firmware"
-
-sed -i 's/^Components: main$/Components: main contrib non-free non-free-firmware/'  "${ROOTFS_DIR}/etc/apt/sources.list.d/debian.sources"
+sed -i 's/^Components: main$/Components: main contrib non-free non-free-firmware/' "${ROOTFS_DIR}/etc/apt/sources.list.d/debian.sources"
