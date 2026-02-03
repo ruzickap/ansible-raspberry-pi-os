11d303f72c3136856dd47aec8950da3a80c85d48
Petr Ruzicka <petr.ruzicka@gmail.com>
2026-02-03 21:29:05 +0100

    feat: update WiFi configuration to use wlan1 - USB WiFi (#268)

--- stage-my/00-netplan/00-run.sh
+++ stage-my/00-netplan/00-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 install -m 600 files/01-wifi.yaml "${ROOTFS_DIR}/etc/netplan/"
-install -m 644 files/wifi-powersave-off.conf "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
+
+echo "dtoverlay=disable-wifi" >> "${ROOTFS_DIR}/boot/firmware/config.txt"
